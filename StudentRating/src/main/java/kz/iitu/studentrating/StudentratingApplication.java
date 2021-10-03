e9bd69001a3a7448eaa3325a3a24133eb0350a26
Your Name <you@example.com>
2021-10-03 22:47:57 +0600

    cds added

--- StudentRating/src/main/java/kz/iitu/studentrating/StudentratingApplication.java
+++ StudentRating/src/main/java/kz/iitu/studentrating/StudentratingApplication.java
@@ -2,12 +2,29 @@ package kz.iitu.studentrating;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StudentratingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentratingApplication.class, args);
     }
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        HttpComponentsClientHttpRequestFactory requestFactory
+                = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(3000);
+
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+
+        return restTemplate;
+    }
 }
