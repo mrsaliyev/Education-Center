97eec9fee6743b654cb256cd7dcfb4c83c64069d
Rsaliyev Madiyar <mrsaliyev@gmail.com>
2021-10-03 19:04:27 +0600

    Discovery Server added

---
+++ Discovery-server/src/test/java/kz/iitu/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.iitu.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
