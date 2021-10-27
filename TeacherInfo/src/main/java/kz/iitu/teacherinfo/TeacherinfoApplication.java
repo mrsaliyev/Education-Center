778a9c0bb15e58098b36517cc4bd7457e5f65c60
Rsaliyev Madiyar <mrsaliyev@gmail.com>
2021-10-27 11:37:32 +0600

    Hystrix add.

--- TeacherInfo/src/main/java/kz/iitu/teacherinfo/TeacherinfoApplication.java
+++ TeacherInfo/src/main/java/kz/iitu/teacherinfo/TeacherinfoApplication.java
@@ -5,12 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+@EnableHystrix
 public class TeacherinfoApplication {
 
     public static void main(String[] args) {
@@ -21,6 +26,17 @@ public class TeacherinfoApplication {
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
+        HttpComponentsClientHttpRequestFactory requestFactory
+                = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(3000);
+
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+
+        return restTemplate;
+    }
+
+    @Bean
+    public RestTemplate restTemplateWithoutLoadBalancer() {
         HttpComponentsClientHttpRequestFactory requestFactory
                 = new HttpComponentsClientHttpRequestFactory();
         requestFactory.setConnectTimeout(3000);
