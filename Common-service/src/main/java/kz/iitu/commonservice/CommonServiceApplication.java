73d4e5c773b8b46b1774858478591ce18509c700
Rsaliyev Madiyar <mrsaliyev@gmail.com>
2021-11-14 23:33:14 +0600

    fix.

---
+++ Common-service/src/main/java/kz/iitu/commonservice/CommonServiceApplication.java
@@ -0,0 +1,16 @@
+package kz.iitu.commonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CommonServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonServiceApplication.class, args);
+    }
+
+}
---
+++ ZuulServer/src/test/java/kz/iitu/zuulserver/ZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.iitu.zuulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
