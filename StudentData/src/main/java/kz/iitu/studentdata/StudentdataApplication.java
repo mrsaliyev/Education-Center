06ca87f2eb35e3392e9e80d99dddc866bc1fe9a4
Aslan Shaizhan <you@example.com>
2021-11-21 20:03:29 +0600

    Security added.

--- StudentData/src/main/java/kz/iitu/studentdata/StudentdataApplication.java
+++ StudentData/src/main/java/kz/iitu/studentdata/StudentdataApplication.java
@@ -1,5 +1,11 @@
 package kz.iitu.studentdata;
 
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -30,6 +36,18 @@ public class StudentdataApplication {
         requestFactory.setConnectTimeout(3000);
 
         RestTemplate restTemplate = new RestTemplate(requestFactory);
+
+        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+
+		credentialsProvider.setCredentials(AuthScope.ANY,
+				new UsernamePasswordCredentials("rest-client", "p@ssword"));
+
+		HttpClient client = HttpClientBuilder
+				.create()
+				.setDefaultCredentialsProvider(credentialsProvider)
+				.build();
+
+		requestFactory.setHttpClient(client);
 
         return restTemplate;
     }
