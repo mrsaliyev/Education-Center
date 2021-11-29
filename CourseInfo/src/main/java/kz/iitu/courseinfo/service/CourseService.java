9ad28b4d605fda36ef3bc2a055891ee43a68b91a
Dias Bolatov <struq2d@gmail.com>
2021-11-30 02:26:33 +0600

    preparing to endturn

--- CourseInfo/src/main/java/kz/iitu/courseinfo/service/CourseService.java
+++ CourseInfo/src/main/java/kz/iitu/courseinfo/service/CourseService.java
@@ -8,34 +8,34 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-    @Service
-    public class CourseService {
-        private final CourseRepo courseRepo;
+@Service
+public class CourseService {
+    private final CourseRepo courseRepo;
 
-        @Autowired
-        public CourseService(CourseRepo courseRepo) {
-            this.courseRepo = courseRepo;
-        }
-
-        public Course addCourse(Course course){
-            return courseRepo.save(course);
-        }
-
-        public List<Course> listAllCourses(){
-            return courseRepo.findAll();
-        }
-
-        public Course updateCourse(Course course){
-            return courseRepo.save(course);
-        }
-
-        public void removeCourse(Long id){
-            courseRepo.deleteCourseById(id);
-        }
-
-        public Course findCourseById(Long id){
-            return courseRepo.findCourseById(id)
-                    .orElseThrow(()-> new CourseNotFoundExceptino("Course with id" + id + " wasn't found"));
-        }
+    @Autowired
+    public CourseService(CourseRepo courseRepo) {
+        this.courseRepo = courseRepo;
     }
+
+    public Course addCourse(Course course){
+        return courseRepo.save(course);
+    }
+
+    public List<Course> listAllCourses(){
+        return courseRepo.findAll();
+    }
+
+    public Course updateCourse(Course course){
+        return courseRepo.save(course);
+    }
+
+    public void removeCourse(Long id){
+        courseRepo.deleteCourseById(id);
+    }
+
+    public Course findCourseById(Long id){
+        return courseRepo.findCourseById(id)
+                .orElseThrow(()-> new CourseNotFoundExceptino("Course with id" + id + " wasn't found"));
+    }
+}
 
