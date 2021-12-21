d9bc66d97ff71dc914a48008ea4d009e8e6656f6
Dias Bolatov <struq2d@gmail.com>
2021-12-21 11:09:36 +0600

    problem with security(will be changed until endTurn)

--- CourseCatalog/src/main/java/kz/iitu/coursecatalog/CourseCatalogApplication.java
+++ CourseCatalog/src/main/java/kz/iitu/coursecatalog/CourseCatalogApplication.java
@@ -2,6 +2,10 @@ package kz.iitu.coursecatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CourseCatalogApplication {
@@ -9,5 +13,16 @@ public class CourseCatalogApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(CourseCatalogApplication.class, args);
+    }
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplateWithoutLoadBalancer() {
+        HttpComponentsClientHttpRequestFactory requestFactory
+                = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(3000);
+
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+
+        return restTemplate;
     }
 }
